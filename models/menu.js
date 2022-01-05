46a8efef9778ad9e1f81c0e68703e37bfedb9753
SGSouthard <sydneyaileensouthard@gmail.com>
2022-01-05 09:50:42 -0800

    updated Menu Schema

--- models/menu.js
+++ models/menu.js
@@ -1,43 +1,77 @@
 const mongoose = require('mongoose');
 
+//Food Items
 const foodItem = new mongoose.Schema({
     name: String,
     description: String,
     price: Number,
 })
-const menuSchema = new mongoose.Schema({
+
+//Drinks
+const drinkItem = new mongoose.Schema({
     name: String,
-    foodItems: [foodItem]
+    description: String,
+    price: Number,
+})
+
+//Food Types (alcohol, lunch, dinner?)
+const foodType = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
 });
 
+//Specifications
+const specificationDrink = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+})
+
+const specificationFood = new mongoose.Schema({
+    name: String,
+    description: String,
+    price: Number,
+})
+
+const menuSchema = new mongoose.Schema({
+    name: String,
+    foodItems: [foodItem],
+    drinkItems: [drinkItem],
+    foodTypes: [foodType],
+    specificationDrinks: [specificationDrink],
+    specificationFood: [specificationFood]
+
+
+});
 const Menu = mongoose.model("Menu", menuSchema);
 
 
-//Example for visual reminders
+//Examples for visual reminders
 
-const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number
-});
+// const productSchema = new mongoose.Schema({
+//     name: String,
+//     price: Number
+// });
 
 
-const orderSchema = new mongoose.Schema({
-    products: [{type: mongoose.Schema.Types.ObjectId, ref: 'Product'}]
-});
+// const orderSchema = new mongoose.Schema({
+//     products: [{type: mongoose.Schema.Types.ObjectId, ref: 'Product'}]
+// });
 
-const Order = mongoose.model('Order', orderSchema);
+// const Order = mongoose.model('Order', orderSchema);
 
-const Product = mongoose.model('Product', productSchema);
+// const Product = mongoose.model('Product', productSchema);
 
-const product = new Product(
-    {
-    name: "holy water",
-    price: 500
-    }
-)
-product.save();
-const order = new Order()
-order.products.push(product)
-order.save();
+// const product = new Product(
+//     {
+//     name: "holy water",
+//     price: 500
+//     }
+// )
+// product.save();
+// const order = new Order()
+// order.products.push(product)
+// order.save();
 
 module.exports = Menu;
